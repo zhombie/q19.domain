31ef55ad4ecaa856abc150a74cebb7af6ea88077
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-07-22 16:24:07 +0600

    Upgrade kotlin = 1.5.21; New Knowledge Base models; Message extensions

--- domain/src/main/java/kz/q19/domain/model/knowledge_base/BaseResponse.kt
+++
@@ -1,9 +0,0 @@
-package kz.q19.domain.model.knowledge_base
-
-import android.os.Parcelable
-import androidx.annotation.Keep
-import kotlinx.parcelize.Parcelize
-
-@Keep
-@Parcelize
-open class BaseResponse : Parcelable
