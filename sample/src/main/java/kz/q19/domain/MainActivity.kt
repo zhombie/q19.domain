ef38b1c58796d70e7d8005a84b8cbfef8b90f489
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-12-22 20:03:35 +0600

    Release v1.5.6

--- sample/src/main/java/kz/q19/domain/MainActivity.kt
+++ sample/src/main/java/kz/q19/domain/MainActivity.kt
@@ -3,9 +3,9 @@ package kz.q19.domain
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kz.q19.domain.model.keyboard.button.TextButton
-import kz.q19.domain.model.user.User
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,4 +13,5 @@ class MainActivity : AppCompatActivity() {
         val button = TextButton("qwerty123")
         println(button)
     }
+
 }
