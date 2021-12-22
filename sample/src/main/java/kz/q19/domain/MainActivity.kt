745150516cb6df45aacc5df355f3dcc9b0fe041c
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-12-22 20:02:08 +0600

    [Sample]

--- sample/src/main/java/kz/q19/domain/MainActivity.kt
+++ sample/src/main/java/kz/q19/domain/MainActivity.kt
@@ -2,6 +2,7 @@ package kz.q19.domain
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kz.q19.domain.model.keyboard.button.TextButton
 import kz.q19.domain.model.user.User
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val user = User(-1, "qwerty123")
-        println(user)
+        val button = TextButton("qwerty123")
+        println(button)
     }
 }
