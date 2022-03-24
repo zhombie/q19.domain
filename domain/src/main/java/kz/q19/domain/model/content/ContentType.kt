0856d69733af2ad8911aff664e6a38eddc93adc7
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2022-03-24 14:03:25 +0600

    New ContentType#from function

--- domain/src/main/java/kz/q19/domain/model/content/ContentType.kt
+++ domain/src/main/java/kz/q19/domain/model/content/ContentType.kt
@@ -8,5 +8,15 @@ enum class ContentType constructor(val key: String) : Parcelable {
     IMAGE("image"),
     VIDEO("video"),
     AUDIO("audio"),
-    DOCUMENT("document")
+    DOCUMENT("document");
+
+    companion object {
+        fun from(key: String?): ContentType? = when (key) {
+            IMAGE.key -> IMAGE
+            VIDEO.key -> VIDEO
+            AUDIO.key -> AUDIO
+            DOCUMENT.key -> DOCUMENT
+            else -> null
+        }
+    }
 }
